0033e7c4806544fb5c39af8d76b16788822acf3e
Milosz <milosz.bedrylo@gmail.com>
2022-03-15 18:06:49 +0100

    added pdf gen to store for test

--- routes/store.js
+++ routes/store.js
@@ -5,6 +5,7 @@ const { date } = require("joi");
 const currentModule = Store;
 const currentModuleName = "store";
 const currentModulePrefix = process.env.STORE_PREFIX;
+const PDFDocument = require('pdfkit');
 
 router.route("/get").get((req, res) => {
     CRUD.getAll(currentModuleName, currentModule, "", false, req.user.roles)
@@ -84,5 +85,44 @@ router.route("/edit/:storeId").patch((req, res) => {
         }
     });
 });
+
+router.route("/pdfraport").get((req, res) => {
+    CRUD.getAll(currentModuleName, currentModule, "", false, req.user.roles)
+        .then((storeItems) => {
+            const pdf = new PDFDocument();
+            res.setHeader("Content-Type", "application/pdf");
+            res.setHeader(
+                "Content-Disposition",
+                "attachment; filename=raport.pdf"
+            );
+            pdf.pipe(res);
+            pdf.fontSize(25).text("Raport");
+            pdf.moveDown();
+            pdf.fontSize(15).text("Data: " + new Date());
+            pdf.moveDown();
+            pdf.fontSize(15).text("Raport z danymi: ");
+            pdf.moveDown();
+            storeItems.forEach((item) => {
+                console.log(item);
+                pdf.fontSize(18).text(item.storeName);
+                pdf.moveDown();
+                pdf.fontSize(15).text("Cena: " + item.storeValue);
+                pdf.moveDown();
+                pdf.fontSize(15).text("Opis: " + item.status);
+                pdf.moveDown();
+                pdf.fontSize(15).text("Lokalizacja: " + item.storeLocation);
+                pdf.moveDown();
+                pdf.fontSize(15).text("Data dodania: " + Date(item.createdAt).toString());
+                pdf.moveDown();
+                pdf.fontSize(15).text("Data modyfikacji: " + Date(item.updatedAt).toString());
+                pdf.moveDown();
+            });
+            //end pdf file 
+            pdf.end();
+
+        })
+        .catch((err) => res.status(500).json("error: " + err));
+});
+
 
 module.exports = router;
